78a34e1914a8b66618fe85551a6a2c35d8d08cca
Victor Molina <Victormolinalvp@gmail.com>
2022-04-23 16:06:01 -0300

    hecho el punto 2-d

--- tp4_2.c
+++ tp4_2.c
@@ -11,19 +11,27 @@ typedef struct Tarea{
 
 void cargarTareas(TAREAS **listaDeTareas, int numeroDeTareas);
 void verificarTareas(TAREAS **listadoDeTareas, TAREAS **RealizarTareas, int numeroDeTareas);
+void mostrarTareas(TAREAS **TareasPendientes, TAREAS **TareasRealizadas, int numeroDeTrareas);
 
 int main(){
+    srand(time(NULL));
     TAREAS **listadoDeTarea, **tareasARealizar;
     int cantidadDeTareas;
 
     printf("Ingresar la cantidad de tareas: \n");
     scanf("%d",&cantidadDeTareas);
 
-    listadoDeTarea = (TAREAS **)malloc(cantidadDeTareas*sizeof(TAREAS));
-    tareasARealizar = (TAREAS **)malloc(cantidadDeTareas*sizeof(TAREAS));
+    listadoDeTarea = (TAREAS **)malloc(cantidadDeTareas*sizeof(TAREAS *));
+    tareasARealizar = (TAREAS **)malloc(cantidadDeTareas*sizeof(TAREAS *));
 
     cargarTareas(listadoDeTarea,cantidadDeTareas);
+
+    for (int i = 0; i < cantidadDeTareas; i++){
+        tareasARealizar[i] = NULL;
+    }
+    
     verificarTareas(listadoDeTarea, tareasARealizar, cantidadDeTareas);
+    mostrarTareas(listadoDeTarea, tareasARealizar, cantidadDeTareas);
 
     getchar();
     getchar();
@@ -34,12 +42,12 @@ void cargarTareas(TAREAS **listaDeTareas, int numeroDeTareas){
     int numeroRandom;
     for (int i = 0; i < numeroDeTareas; i++){
 
-        listaDeTareas[i] = (TAREAS *)malloc(sizeof(TAREAS));
+        listaDeTareas[i] = (TAREAS *)malloc(sizeof(TAREAS)*1);
 
         numeroRandom = rand()%numeroDeTareas + 1;
         listaDeTareas[i]->TareaID = numeroRandom;
 
-        listaDeTareas[i]->Description = (char *)malloc(100*sizeof(char));
+        listaDeTareas[i]->Description = (char *)malloc(100*sizeof(char)+1);
         numeroRandom = rand()%3;
         switch (numeroRandom){
         case 0:
@@ -54,28 +62,40 @@ void cargarTareas(TAREAS **listaDeTareas, int numeroDeTareas){
         }
 
         listaDeTareas[i]->Duracion = rand() % 91 + 10;
-
-        free(listaDeTareas[i]->Description);
     }
 }
 
 void verificarTareas(TAREAS **TareasPendientes, TAREAS **RealizarTareas, int numeroDeTareas){
     int auxiliar;
     for (int i = 0; i < numeroDeTareas; i++){
-        RealizarTareas[i] = (TAREAS *)malloc(sizeof(TAREAS));
-
-        printf("\n------ Tarea numero %d - %s - Duracion: %d ------",TareasPendientes[i]->TareaID, TareasPendientes[i]->Description, TareasPendientes[i]->Duracion);
-
         do
         {
-            printf("\n¿Se realizo esta tarea?(1:Si, 0:No):");
+            printf("\nSe realizo esta tarea de %s?(1:Si, 0:No):",TareasPendientes[i]->Description);
             scanf("%d",&auxiliar);
         } while (auxiliar != 1 && auxiliar !=0);
         
         if (auxiliar == 1){
             RealizarTareas[i] = TareasPendientes[i];
             TareasPendientes[i] = NULL;
-        }   
+        }
     }   
 }
 
+void mostrarTareas(TAREAS **TareasPendientes, TAREAS **TareasRealizadas, int numeroDeTareas){
+    printf("\n====== TAREAS PENDIENTES ======\n");
+    for (int i = 0; i < numeroDeTareas; i++){
+        if (TareasPendientes[i] != NULL){
+            printf("\nID de la tarea: %d",TareasPendientes[i]->TareaID);
+            printf("\nTarea a realizar: %s", TareasPendientes[i]->Description);
+            printf("\nDuracion de la tarea: %d horas",TareasPendientes[i]->Duracion);
+        }
+    }
+    printf("\n====== TAREAS REALIZADAS ======\n");
+    for (int i = 0; i < numeroDeTareas; i++){
+        if (TareasRealizadas[i] != NULL){
+            printf("\nID de la tarea: %d",TareasRealizadas[i]->TareaID);
+            printf("\nTarea a realizar: %s",TareasRealizadas[i]->Description);
+            printf("\nDuracion de la tarea: %d horas",TareasRealizadas[i]->Duracion);
+        }
+    }
+}
